1f15ea862dd25da37d97c27055fd17205ab01fce
PRATHAMESHG1234 <ghorpadeprathamesh2411@gmail.com>
2023-10-17 17:36:19 +0530

    added style

--- calculator.js
+++ calculator.js
@@ -1,38 +1,35 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const fs = require("file-system")
-
 const app = express();
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set("view engine", "ejs");
+app.get("/bmiCalculator", function (req, res) {
+  res.sendFile(__dirname + "/bmiCalculator.html");
+});
 
+app.post("/bmiCalculator", function (req, res) {
+  var height = Number(req.body.Height);
+  var weight = Number(req.body.Weight);
 
-app.get("/bmiCalculator",function (req,res) {
-    res.sendFile(__dirname+"/bmiCalculator.html")
-})
+  var result = Math.round(weight / (height / 100) ** 2);
 
+  res.render(__dirname + "/result", { result: result });
+});
 
-app.post("/bmiCalculator",function (req,res) {
-    var height = Number(req.body.Height);
-    var weight = Number(req.body.Weight);
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/index.html");
+});
 
-    var result = Math.round(weight / ((height/100)**2));
-    res.send("the calculated result is "+ result);
-})
-app.get("/",function (req,res) {
-    res.sendFile(__dirname +"/index.html")
-})
+app.post("/", function (req, res) {
+  var num1 = Number(req.body.Num1);
+  var num2 = Number(req.body.Num2);
 
+  var result = num1 + num2;
 
-app.post("/",function (req,res) {
-    var num1 = Number(req.body.Num1);
-    var num2 = Number(req.body.Num2);
+  res.render(__dirname + "/result", { result: result });
+});
 
-    var result = num1 + num2;
-    res.send("the calculated result is "+ result);
-})
-
-
-const port =  3000;
-app.listen(port,function () {
-    console.log(`server listening on port ${port}`)  
+const port = 3000;
+app.listen(port, function () {
+  console.log(`Server listening on port ${port}`);
 });
